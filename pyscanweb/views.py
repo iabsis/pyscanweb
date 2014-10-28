5e4414a4813600e2217831be330394609a1035c1
gilles hemmerle <gilles.h@iabsis.com>
2014-10-28 14:40:26 +0100

    Ajout structure web

---
+++ pyscanweb/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django.shortcuts import render
+
+
+def scanpage(request):
+    template = loader.get_template('pyscanweb/index.html')
+    context = RequestContext(request, {
+        'test': 'bonjour',
+    })
+    return HttpResponse(template.render(context))
