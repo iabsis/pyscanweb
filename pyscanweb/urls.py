35b5c9a8c64334dfca5ccb93dbb6c3a5850991ec
gilles hemmerle <gilles.h@iabsis.com>
2014-10-30 11:32:57 +0100

    Removed a test url

--- pyscanweb/urls.py
+++ pyscanweb/urls.py
@@ -12,6 +12,5 @@ urlpatterns = patterns('',
     url(r'json/launch_scanner$', views.json_launch_scanner, name="json_launch_scanner"),
     url(r'last_scanned_image$', views.get_last_scanned_image, name="last_scanned_image"),
     url(r'scanned_image/(?P<id>[0-9]{1,3})$', views.get_session_image_id, name="get_scanned_image_id"),
-    url(r'test$', views.test, name="test"),
     url(r'^$', views.scanpage, name="scanpage"),
 )
