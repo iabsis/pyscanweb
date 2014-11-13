26549c628a5fa2cb83b4ae08f7775f09f2c24304
Olivier Bitsch <olivier.bitsch@gmail.com>
2014-11-13 16:06:01 +0100

    * Fix close scanner.

--- pyscanweb/views.py
+++ pyscanweb/views.py
@@ -50,23 +50,12 @@ def json_launch_scanner(request):
         return HttpResponse(json.dumps({'error' : 'Invalid post data'}), content_type="application/json")
 
     try:
-        print "init"
         sane.init()
-        print "open device :", device_name
         scanner = sane.open(device_name)
-        #scanner.get_options()
-        print "set parameters"
-        pprint.pprint(scanner)
-        print "set resolution", device_resolution
         scanner.resolution = device_resolution
-        print type(device_source)
-        print "set scanner source", str(device_source)
         scanner.source = str(device_source)
-        print "set scanner mode", str(device_mode)
         scanner.mode = str(device_mode)
-        print "init finalized"
-    except Exception as e:
-        pprint.pprint(e)
+    except:
         return HttpResponse(json.dumps({'error' : 'Unable to initialize the scanner'}), content_type="application/json")
 
 
@@ -74,30 +63,23 @@ def json_launch_scanner(request):
     nb_images = 0
     try:
         while True:
-            print "get image"
             image = scanner.scan()
-            print "passe1"
             tf = tempfile.NamedTemporaryFile(prefix="pyscanweb_")
-            print "passe2"
             image.save(tf.name + ".jpg", 'JPEG')
-            print "passe3"
             sessionManager.session_add_image(tf.name + ".jpg")
-            print "passe4"
             nb_images += 1
             print nb_images
             if not multipage:
                 break;
     except:
-        print "fucke"
         pprint.pprint(sys.exc_info())
         if nb_images == 0:
             return HttpResponse(json.dumps({'error' : 'Unable to scan documents'}), content_type="application/json")
+    finally:
+        scanner.close()
 
-
-    print "generate json"
     linksList = [];
     images_list = sessionManager.get_session_images_list()
-    pprint.pprint( images_list)
     total_image_count = len(images_list)
     print nb_images
     for i in range(total_image_count-nb_images, total_image_count):
@@ -134,7 +116,6 @@ def get_session_image_id(request, id):
 
 def generate_pdf(request):
     images_list = request.POST.getlist("images_list")
-    pprint.pprint(images_list)
     sessionManager = SessionScanedImagesManager(request)
 
     pdf_filename = tempfile.NamedTemporaryFile(prefix="pyscanweb_pdf_").name + ".pdf"
@@ -143,7 +124,6 @@ def generate_pdf(request):
 
     for image in images_list:
         sessionImage = sessionManager.get_image(int(image))
-        pprint.pprint(sessionImage)
         f.add_page()
         f.image(sessionImage, x=0, y=0, w=210, h=0)
 
